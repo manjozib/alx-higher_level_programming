d4ae9b9d484cb70fd8639633c1ced90078c35e19
systemsAnalysts <manjozib@gmail.com>
2022-10-17 12:11:16 +0200

    correcting first task

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module 0-rectangle
+Defines an empty Rectangle class.
+"""
 class Rectangle:
-    """an empty class Rectangle that defines a rectangle:"""
+    """an empty class"""
     pass
