12478c6b9b1857394b5f412e9361ef4bf06878d9
systemsAnalysts <manjozib@gmail.com>
2022-10-25 20:57:11 +0200

    correcting task 10

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,41 +1,30 @@
 #!/usr/bin/python3
+"""Module 11-student.
+Creates a Student class.
+"""
+
+
 class Student:
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
     """
-    a class Student that defines a student by:
 
-    Public instance attributes:
-    first_name
-    last_name
-    age
-
-    Instantiation with first_name, last_name and age:
     def __init__(self, first_name, last_name, age):
-    Public method def to_json(self):
-    that retrieves a dictionary representation of a Student instance
-    """
-    def __init__(self, first_name, last_name, age):
-        """
-        init method that initialise attributes of Student class
+        """Initializes the Student instance."""
 
-        Args:
-            first_name (str):
-            last_name (str):
-            age (str):
-
-        Return:
-            dictionary representation of a Student instance
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        a function that retrieves a dictionary representation of a
-        Student instance
+        """Retrieves a dictionary representation
+        of a Student instance.
 
-        Return:
-            dictionary representation of a Student instance
+        Returns: the dict representation of the instance.
         """
-        dict_ = self.__dict__
-        return dict_
+
+        return self.__dict__
